172d1755ff4b1fcdafa7a4d22728e9c1d366f3f0
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-19 20:50:28 +0100

    finished the third problem
    this was a bit easier, had to write a function that reversed a string.
    first turning it into an array and then reversing it and then turning it back into a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  // word = word.split("");  // split the string into an array
+  // word = word.reverse();  // reverse the array
+  // word = word.join("");  // join the array back into a string
+  return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
