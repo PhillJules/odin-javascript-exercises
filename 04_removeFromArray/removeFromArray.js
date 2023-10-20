a42db11c3e0b7f4e9c125b4f73d2ecd4ab7abad0
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-20 01:19:53 +0100

    finished the 4th exercise
    this was a little more challenging than the previous ones, but I realized
    that i just need to relax and take my time. use psuedo code and break it down

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  // create a new empty array
+  const newArray =[];
+  // use forEach to go through the array
+  array.forEach(element => {
+    // need an if statement that checks if the element is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(element)){
+      // push every element into the new array
+      newArray.push(element)
+    }
+  });
+  // and return that array
+  return newArray;
 };
 
 // Do not edit below this line
