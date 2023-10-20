43eceb45e0e2c6bef687ff81f3fe8727cc214fa9
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-20 20:08:10 +0100

    finished exercise 7
    was a lot easier than the previous one, all i had to do was convert
    the fahrenheit to celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+ const celConversion = (fahrenheit - 32) * 5/9;
+ return Math.round(celConversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahConversion = (celsius * 9/5) + 32;
+  return Math.round(fahConversion * 10) / 10;
 };
 
 // Do not edit below this line
