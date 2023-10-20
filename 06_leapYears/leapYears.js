eefd83a0bd84dcc7be2dd2a8d1b44c88d6f9f972
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-10-20 19:36:14 +0100

    exercise 6 is finished
    this was alot easier than the previous exercises. I think I'm getting the hang of this.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+// make a variable to store the result
+let isLeapYear = false;
+// an if statement to check if the year is divisible by 4  or 400
+if ((year % 4 === 0 && year % 100 !== 0 )|| year % 400 === 0) {
+  // if it is, set the result to true
+  return isLeapYear = true;
+}else {
+  return isLeapYear = false;
+}
+// return the result
 };
 
 // Do not edit below this line
