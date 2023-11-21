ddfd06875bc38e618004f2cc658e804b97f4b080
Phillipe John Jules <phillipejohnjules@gmail.com>
2023-11-21 01:53:48 +0000

    finished the calculator lesson
    just finished the calculator lesson now on to the palindrome lesson

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	let sum = n1 + n2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+ let difference = n1 - n2;
+  return difference;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 
 };
 
-const power = function() {
-	
+const multiply = function(array) {
+ return array.reduce((product, current) => product * current);
 };
 
-const factorial = function() {
-	
+const power = function(base, exponent) {
+
+  return base ** exponent
+};
+
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+
+    return n * factorial(n - 1)
+  }
+
+
 };
 
 // Do not edit below this line
