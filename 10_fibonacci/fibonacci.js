b1aa8e1c5d8057555c4ca6ba9cdef72ec2f4ac9e
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-01-08 23:55:47 +0000

    finished fibonacci  exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  let i ;
+  let fib = [1, 1];
+  count = parseInt(count);  // convert string to number
+
+  for (i = 2; i < count; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1 ];
+
+
+  }
+
+  if (count < 1 ) {
+    return "OOPS";
+  }
+
+  return fib[fib.length - 1];
 
 };
 
