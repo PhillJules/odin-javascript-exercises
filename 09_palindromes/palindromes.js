4ca41f8688b1a971ce3e4b181e6d328ca6c3470e
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-01-08 23:04:46 +0000

    finished
    completed the palindrome challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // let num = str;
+  // if (typeof num !== 'string') {
+  //   num = num.toString();
+  // }
+  // return num.split("").reverse().join("") === num;
+  if (typeof str !== 'string') {
+    str = str.toString();
+  }
+  let newStr = str.toLowerCase().replace( /[^\w]/g, '').split("").reverse().join("");
+
+  return str.toLowerCase().replace( /[^\w]/g, '') === newStr;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
